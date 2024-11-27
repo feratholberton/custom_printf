096d6bd9cd5126950fd941f2666dd02d7169d701
Fer <fernando.falcon@holbertonstudents.com>
2024-11-27 19:56:15 +0000

    _printf definition

--- main.h
+++ main.h
@@ -5,8 +5,5 @@
 #include <unistd.h>
 
 int _printf(const char *format, ...);
-int handle_specifier(char specifier, va_list args);
-int write_char(char c);
-int write_string(const char *str);
 
 #endif
