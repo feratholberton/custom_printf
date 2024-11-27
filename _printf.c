bc1d0cbd0a7d48df083d1a642b9360b328da2c97
Fer <fernando.falcon@holbertonstudents.com>
2024-11-27 18:11:55 +0000

    check for a null format string

--- _printf.c
+++ _printf.c
@@ -6,45 +6,19 @@
  *
  * Return: Number of characters printed, or -1 on error
  */
-int _printf(const char *format, ...) {
-    int char_count = 0;
-    const char *p;
-    int spec_count;
 
-    va_list args;
-    va_start(args, format);
+int _printf(const char *format, ...)
+{
+	int char_count = 0;
+	const char *p;
 
-    if (format == NULL) {
-        return -1;
-    }
+	if (format == NULL || *format == '\0')
+		return -1;
 
-	for (p = format; *p != '\0'; p++) {
-		/* find specifier */
-		if (*p == '%') {
-			p++;
-            if (*p == '\0') {
-                va_end(args);
-                return -1;
-            }
-            spec_count = handle_specifier(*p, args);
-            if (spec_count == -1) {
-                va_end(args);
-                return -1;
-            }
-            char_count += spec_count;
-        } 
-		/* Regular character */
-		else 
-		{
-            if (write_char(*p) == -1) {
-                va_end(args);
-                return -1;
-            }
-            char_count++;
-        }
-    }
+	for (p = format; *p != '\0'; p++)
+	{
+		char_count++;
+	}
 
-    va_end(args);
-    return char_count;
+	return char_count;
 }
-
