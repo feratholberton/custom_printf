20d54a94c9ba82c09f7c916a71e297bd9f3db449
Fer <fernando.falcon@holbertonstudents.com>
2024-11-29 15:10:44 +0000

    Handle % literal and unknown identifiers

--- _printf.c
+++ _printf.c
@@ -15,10 +15,34 @@ int _printf(const char *format, ...)
 	if (format == NULL)
 		return -1;
 
+	/* Start iteration over format string until it finds the null terminator*/
 	for (p = format; *p != '\0'; p++)
 	{
-		write(1, p, 1);
-		char_count++;
+		/* Checks for % sing | a.k.a. specifier */
+		if (*p == '%')
+		{
+			/* Move pointer to next position */
+			p++;
+
+			/* Handle literal % sing */
+			if (*p == '%')
+			{
+				write(1, "%", 1);
+				char_count++;
+			}
+			/* Handle unknow specifier */
+			else
+			{
+				write(1, "%", 1);
+				write(1, p, 1);
+				char_count += 2;
+			}
+		}
+		else
+		{
+			write(1, p, 1);
+			char_count++;
+		}
 	}
 
 	return char_count;
